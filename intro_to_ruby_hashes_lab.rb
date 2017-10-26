fc4cd9c0fdc510c91108c1b4e0e40c41ed4af886
Min Kim <mk4555@nyu.edu>
2017-10-26 18:04:33 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,10 +18,6 @@ def monopoly_with_second_tier
     rent_in_dollars: {}
     }
   }
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:names] = {}
-  monopoly[:railroads][:rent_in_dollars] = {}
-  monopoly
 end
 
 def monopoly_with_third_tier
